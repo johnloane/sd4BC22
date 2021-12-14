dc35fc00610d22f11100ee40e9cd08ab119ddf6c
John Loane <john.loane@dkit.ie>
2021-12-14 09:06:53 +0000

    Training a model to generate steering angle for car

---
+++ bc.py
@@ -0,0 +1,140 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import tensorflow.keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Dense, Flatten, Convolution2D, MaxPooling2D
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+import cv2
+import pandas as pd
+import ntpath
+import random
+
+
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    return tail
+
+
+def load_steering_img(datadir, df):
+    image_path = []
+    steering = []
+    for i in range(len(data)):
+        indexed_data = data.iloc[i]
+        center, left, right = indexed_data[0], indexed_data[1], indexed_data[2]
+        image_path.append(os.path.join(datadir, center.strip()))
+        steering.append(float(indexed_data[3]))
+        image_path.append(os.path.join(datadir, left.strip()))
+        steering.append(float(indexed_data[3]))
+        image_path.append(os.path.join(datadir, right.strip()))
+        steering.append(float(indexed_data[3]))
+    image_paths = np.asarray(image_path)
+    steering = np.asarray(steering)
+    return image_paths, steering
+
+
+def img_preprocess(img):
+    img = mpimg.imread(img)
+    #Crop the image
+    img = img[60:135, :, :]
+    # Convert color to yuv y-brightness, u,v chrominants(color)
+    # Recommend in the NVIDIA paper
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    # Apply Gaussian Blur
+    # As suggested by NVIDIA paper
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img/255
+    return img
+
+
+def nvidia_model():
+    model = Sequential()
+
+
+datadir = 'C:\\Users\\loanej\\dev\\Smart Technologies\\beta_simulator_windows\\BCData'
+columns = ['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed']
+data = pd.read_csv(os.path.join(datadir, 'driving_log.csv'), names = columns)
+pd.set_option('max_columns', 7)
+print(data.head())
+
+data['center'] = data['center'].apply(path_leaf)
+data['left'] = data['left'].apply(path_leaf)
+data['right'] = data['right'].apply(path_leaf)
+
+num_bins = 25
+samples_per_bin = 300
+hist, bins = np.histogram(data['steering'], num_bins)
+print(bins)
+center = (bins[:-1] + bins[1:])*0.5
+plt.bar(center, hist, width=0.05)
+plt.plot((np.min(data['steering']), np.max(data['steering'])), (samples_per_bin, samples_per_bin))
+plt.show()
+
+print('Total data: ', len(data))
+
+remove_list = []
+for j in range(num_bins):
+    list_ = []
+    for i in range(len(data['steering'])):
+        if bins[j] <= data['steering'][i] <= bins[j + 1]:
+            list_.append(i)
+    list_ = shuffle(list_)
+    list_ = list_[samples_per_bin:]
+    remove_list.extend(list_)
+
+print('Remove: ', len(remove_list))
+data.drop(data.index[remove_list], inplace=True)
+print('Remaining: ', len(data))
+
+hist, _ = np.histogram(data['steering'], num_bins)
+plt.bar(center, hist, width=0.05)
+plt.plot((np.min(data['steering']), np.max(data['steering'])), (samples_per_bin, samples_per_bin))
+plt.show()
+
+print(data.iloc[1])
+
+image_paths, steering = load_steering_img(datadir + '/IMG', data)
+X_train, X_valid, y_train, y_valid = train_test_split(image_paths, steering, test_size=0.2, random_state=6)
+print('Training samples: {}\nValid Samples: {}'.format(len(X_train), len(X_valid)))
+
+fig, axes = plt.subplots(1, 2, figsize=(12,4))
+axes[0].hist(y_train, bins=num_bins, width=0.05, color='blue')
+axes[0].set_title('Training set')
+axes[1].hist(y_valid, bins=num_bins, width=0.05, color='red')
+axes[1].set_title('Validation set')
+plt.show()
+
+image = image_paths[100]
+original_image = mpimg.imread(image)
+preprocessed_image = img_preprocess(image)
+fig, axes = plt.subplots(1, 2, figsize=(15,10))
+fig.tight_layout()
+axes[0].imshow(original_image)
+axes[0].set_title('Original Image')
+axes[1].imshow(preprocessed_image)
+axes[1].set_title('Preprocessed Image')
+plt.show()
+
+X_train = np.array(list(map(img_preprocess, X_train)))
+X_valid = np.array(list(map(img_preprocess, X_valid)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
